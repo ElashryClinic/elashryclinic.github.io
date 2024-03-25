94f6ee850f45944f3bca08fac91e9a38874c3ca7
ElashryClinic <elashryclinic@gmail.com>
2024-03-25 23:26:52 +0200

    Ye

--- index.js
+++
@@ -1,9 +0,0 @@
-function sendToWhatsApp() {
-    const recipient = '01029346225';
-    const name = document.getElementById('الاسم الثلاثي').value;
-    const phone = document.getElementById('رقم الهاتف').value;
-    const specialty = document.querySelector('select[name="التخصص"]').value;
-    const message = `الاسم: ${name}\nرقم الهاتف: ${phone}\nالتخصص: ${specialty}`;
-
-    window.location.href = `https://api.whatsapp.com/send?phone=${recipient}&text=${encodeURIComponent(message)}`;
-}
