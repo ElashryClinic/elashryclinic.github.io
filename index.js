2f8b2c8330924d8644d2a654d59464673b58c3f0
ElashryClinic <elashryclinic@gmail.com>
2024-03-31 19:33:53 +0200

    Return to Gmail.

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function sendEmail()
       Host: "smtp.elasticemail.com",
       Username: "elashryclinic@gmail.com",
       Password: "719D95FB23012B20FE8CFA4667E8C3369096",
-      To: "elashryclinic@outlook.com",
+      To: "elashryclinic@gmail.com",
       From: "elashryclinic@gmail.com",
       Subject : "عملية حجز جديدة من خلال موقع عيادات العشري",
       Body : `<h4>حدثت عملية حجز جديدة بموقع عيادات العشري بالتفاصيل الآتية: </h4><br><h2>الاسم:</h2><h1>${document.getElementById('الاسم الثلاثي').value}</h1><br><h2>رقم الهاتف:</h2><h1>${document.getElementById('رقم الهاتف').value}</h1><br><h2>التخصص:</h2><h1>${document.getElementById('التخصص').options[document.getElementById('التخصص').selectedIndex].text}</h1>`
